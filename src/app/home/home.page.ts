ae615d8e4ba6bb7b48da9845b8985e53f9c5390f
MMAGES2006 <22170@portalcedi.edu.mx>
2025-02-02 23:42:47 -0600

    typos y pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,29 +1,151 @@
-import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { PokemonService } from '../pokemon.service'
+// import { HttpClientModule } from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { PokemonService } from '../pokemon.service'
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: 'home.page.html',
+//   styleUrls: ['home.page.scss'],
+//   standalone: false,
+// })
+// export class HomePage {
+
+//   listPokemons: any = [];
+
+//   constructor(private pokeService: PokemonService) {}
+
+//   ngOnInit()
+//   {
+//     this.pokeService.getListPokemon().subscribe((data) => {
+//       this.listPokemons = data.results
+//       console.log(data.results)
+//     })
+//   }
+
+//   handleDetail(url: any)
+//   {
+//     console.log(url)
+//   }
+// }
+
+
+
+
+
+
+// import { HttpClientModule } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import { PokemonService } from '../pokemon.service';
+// import { forkJoin, Observable } from 'rxjs';
+// import { switchMap } from 'rxjs/operators';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.page.html',
+//   styleUrls: ['./home.page.scss'],
+//   standalone: false,
+
+// })
+// export class HomePage implements OnInit {
+//   listPokemons: any[] = [];
+//   cards: any[] = [];
+//   isExpanded = false;
+
+//   selectedCardIndex: number | null = null; // Estado del índice de la tarjeta seleccionada
+
+//   constructor(private pokemonService: PokemonService) {}
+
+//   toggleCard(ind: number) {
+//     // Si la tarjeta ya está abierta, la cerramos. Si no, la abrimos
+//     if (this.selectedCardIndex === ind) {
+//       this.selectedCardIndex = null;
+//     } else {
+//       this.selectedCardIndex = ind;
+//     }
+//   }
+
+//   ngOnInit() {
+//     this.pokemonService.getListPokemon().pipe(
+//       switchMap((data: any) => {
+//         const pokemonDetailsRequests: Observable<any>[] = data.results.map((pokemon: any) => this.pokemonService.getDetailsPokemon(pokemon.url));
+//         return forkJoin(pokemonDetailsRequests);
+//       })
+//     ).subscribe(
+//       (pokemonDetails: any[]) => {
+//         this.listPokemons = pokemonDetails.map(pokemon => ({
+//           name: pokemon.name,
+//           type: pokemon.types.map((t: any) => t.type.name).join(', '),
+//           url: pokemon.url,
+//         }));
+//       },
+//       error => {
+//         console.error('Error al obtener la lista de Pokémon', error);
+//       }
+//     );
+//   }
+
+//   handleDetail(url: string) {
+//     console.log('Pokémon URL:', url);
+//     // Aquí puedes redirigir a la página de detalles o realizar alguna acción adicional
+//   }
+
+  
+// }
+
+
+
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+import { forkJoin, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  listPokemons: any[] = []; 
+  selectedCardIndex: number | null = null; 
 
-  listPokemons: any = [];
+  constructor(private pokemonService: PokemonService) {}
 
-  constructor(private pokeService: PokemonService) {}
-
-  ngOnInit()
-  {
-    this.pokeService.getListPokemon().subscribe((data) => {
-      this.listPokemons = data.results
-      console.log(data.results)
-    })
+  toggleCard(index: number) {
+    if (this.selectedCardIndex === index) {
+      this.selectedCardIndex = null;
+    } else {
+      this.selectedCardIndex = index;
+    }
   }
 
-  handleDetail(url: any)
-  {
-    console.log(url)
+  ngOnInit() {
+    this.pokemonService.getListPokemon().pipe(
+      switchMap((data: any) => {
+        const pokemonDetailsRequests: Observable<any>[] = data.results.map((pokemon: any) => 
+          this.pokemonService.getDetailsPokemon(pokemon.url)
+        );
+        return forkJoin(pokemonDetailsRequests);
+      })
+    ).subscribe(
+      (pokemonDetails: any[]) => {
+        this.listPokemons = pokemonDetails.map(pokemon => ({
+          name: pokemon.name,
+          type: pokemon.types.map((t: any) => t.type.name).join(', '),
+          url: pokemon.url,
+          description: `Este es un Pokémon de tipo ${pokemon.types.map((t: any) => t.type.name).join(', ')}`,
+        }));
+      },
+      error => {
+        console.error('Error al obtener la lista de Pokémon', error);
+      }
+    );
+  }
+
+  handleDetail(url: string) {
+    console.log('Pokémon URL:', url);
   }
 }
--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,25 +1,50 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable, ObservedValueOf } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PokemonService {
+
+//   private apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0, https://pokeapi.co/api/v2/type/?limit=18&offset=0'
+//   private limit = 151;
+//   private offset = 0;
+
+//   constructor(private http: HttpClient) { }
+
+//   getListPokemon(): Observable<any>
+//   {
+//     return this.http.get(`${this.apiUrl}?limit=${this.limit}`)
+//   }
+
+//   getDetailsPokemon(urlPokemon: any): Observable<any>
+//   {
+//     return this.http.get(urlPokemon);
+//   }
+// }
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, ObservedValueOf } from 'rxjs';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  private apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0'
+  private pokemonApiUrl = 'https://pokeapi.co/api/v2/pokemon';
   private limit = 151;
   private offset = 0;
 
   constructor(private http: HttpClient) { }
 
-  getListPokemon(): Observable<any>
-  {
-    return this.http.get(`${this.apiUrl}?limit=${this.limit}`)
+  getListPokemon(): Observable<any> {
+    return this.http.get(`${this.pokemonApiUrl}/?limit=${this.limit}&offset=${this.offset}`);
   }
 
-  getDetailsPokemon(urlPokemon: any): Observable<any>
-  {
+  getDetailsPokemon(urlPokemon: string): Observable<any> {
     return this.http.get(urlPokemon);
   }
 }
